633fa71d3b7d4d2466d192f7268f45aa56b81d5d
Baptiste Durand <baptiste.durand@enpc.fr>
2019-01-18 15:10:53 +0100

    New geometry transformation : Rotation in 3D

--- geometry.py
+++ geometry.py
@@ -1381,4 +1381,121 @@ def translation_basis(pt_coord, vect):
     vect = np.asarray(vect)
     return pt_coord + vect
 translation = geo_transformation_factory(translation_basis)
-#? Avis d'Arthur sur cette façon de définir les opérations géométriques ?
+
+def rotation_matrix(angle, direction, point=None):
+    """
+    Return matrix to rotate about axis defined by point and direction.
+    
+    Returns
+    -------
+    4×4 numpy.array
+        Matrix representation of the rotation.
+    
+    From : transformations.py, https://www.lfd.uci.edu/~gohlke/
+    Copyright (c) 2006-2019, Christoph Gohlke
+    Copyright (c) 2006-2019, The Regents of the University of California
+    Produced at the Laboratory for Fluorescence Dynamics
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+    * Neither the name of the copyright holder nor the names of its
+    contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+    ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+    LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
+    
+
+    >>> R = rotation_matrix(math.pi/2, [0, 0, 1], [1, 0, 0])
+    >>> numpy.allclose(numpy.dot(R, [0, 0, 0, 1]), [1, -1, 0, 1])
+    True
+    >>> angle = (random.random() - 0.5) * (2*math.pi)
+    >>> direc = numpy.random.random(3) - 0.5
+    >>> point = numpy.random.random(3) - 0.5
+    >>> R0 = rotation_matrix(angle, direc, point)
+    >>> R1 = rotation_matrix(angle-2*math.pi, direc, point)
+    >>> is_same_transform(R0, R1)
+    True
+    >>> R0 = rotation_matrix(angle, direc, point)
+    >>> R1 = rotation_matrix(-angle, -direc, point)
+    >>> is_same_transform(R0, R1)
+    True
+    >>> I = numpy.identity(4, numpy.float64)
+    >>> numpy.allclose(I, rotation_matrix(math.pi*2, direc))
+    True
+    >>> numpy.allclose(2, numpy.trace(rotation_matrix(math.pi/2, direc, point)))
+    True
+    """
+
+    sina = math.sin(angle)
+    cosa = math.cos(angle)
+    direction = unit_vect(direction[:3])
+    # rotation matrix around unit vector
+    R = np.diag([cosa, cosa, cosa])
+    R += np.outer(direction, direction) * (1.0 - cosa)
+    direction *= sina
+    R += np.array([[ 0.0,         -direction[2],  direction[1]],
+                      [ direction[2], 0.0,          -direction[0]],
+                      [-direction[1], direction[0],  0.0]])
+    M = np.identity(4)
+    M[:3, :3] = R
+    if point is not None:
+        # rotation not around origin
+        point = np.array(point[:3], dtype=np.float64, copy=False)
+        M[:3, 3] = point - np.dot(R, point)
+    print(M)
+    return M
+
+def rotation_basis(pt_coord, angle, direction, point=None):
+    """
+    Apply a rotation operation to a point.
+    The rotation axis is defined by a point and a direction.
+
+    Parameters
+    ----------
+    pt_coord : 1-D array-like
+        3 coordinates of the point to which the rotation is applied.
+    angle : float
+        Rotation angle
+    direction : 1-D array
+        Direction of the axis of rotation, 3 component vector
+    point : 1-D array, optional
+        3 oordinates of a point through which the axis of rotation passes.
+        The default is None, which implies a rotation about the origin (0.,0.,0.)
+
+    Returns
+    -------
+    rot_coord
+        3 coordinates of the rotated point.
+    
+    >>> v1 = np.array((0.,1.,0.))
+    >>> v3 = rotation_basis(v1, math.pi/2, [0, 0, 1], [1, 0, 0]))
+    >>> np.allclose(np.array((0.,-1.,0.)),v3))
+    True
+    """
+
+    R = rotation_matrix(angle, direction, point)
+    x = np.pad(pt_coord, (0,1), 'constant', constant_values=(1.))
+    y = np.dot(R, x)
+    rot_coord = y[:3]
+    return rot_coord
+rotation = geo_transformation_factory(rotation_basis)
+#TODO : tester cette fonction
