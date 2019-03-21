0312fab93adf893655158bd79a6bcb6c037e2624
Baptiste Durand <baptiste.durand@enpc.fr>
2019-03-21 13:13:18 +0100

    Setup files for install

--- HO_homog/__init__.py
+++ HO_homog/__init__.py
@@ -1,3 +1,13 @@
 # coding: utf8
 
+"""
+#TODO : Décrire le package
+"""
+
 #? http://sametmax.com/vous-pouvez-mettre-du-code-dans-__init__-py/
+
+#TODO : faciliter des imports 
+#? http://sametmax.com/creer-un-setup-py-et-mettre-sa-bibliotheque-python-en-ligne-sur-pypi/
+
+ 
+__version__ = "0.1"
---
+++ setup.py
@@ -0,0 +1,11 @@
+# coding: utf8
+"""
+Created on 21/03/2019
+@author: baptiste
+
+sources : http://sametmax.com/vive-setup-cfg-et-mort-a-pyproject-toml/,
+http://sametmax.com/creer-un-setup-py-et-mettre-sa-bibliotheque-python-en-ligne-sur-pypi/
+"""
+
+import setuptools
+setuptools.setup()
