d0f3e2aac32dada293ec61dfba7e77492112c264
Baptiste Durand <baptiste.durand@enpc.fr>
2019-04-26 17:08:43 +0200

    improve __init__ file syntax

--- ho_homog/__init__.py
+++ ho_homog/__init__.py
@@ -3,25 +3,17 @@
 """
 #TODO : Décrire le package
 """
- 
-__version__ = "0.1"
-
-GEO_TOLERANCE = 1e-12
-
-from . import geometry
-from . import homog2d
-from . import materials
-from . import mesh_generate_2D
-from . import mesh_tools
-from . import part
-from . import toolbox_FEniCS
-from . import full_scale_pb
 
 import logging
 from logging.handlers import RotatingFileHandler
 from pathlib import Path
+__version__ = "0.1"
+
+GEO_TOLERANCE = 1e-12
 
 
+from . import (full_scale_pb, geometry, homog2d, materials, mesh_generate_2D,
+               mesh_tools, part, toolbox_FEniCS)
 
 log_level = logging.DEBUG
 log_path = Path('~/ho_homog_log/activity.log').expanduser()
@@ -30,16 +22,18 @@ if not log_path.parent.exists():
 pckg_logger = logging.getLogger(__name__)
 formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(name)s :: %(message)s',"%Y-%m-%d %H:%M:%S")
 
+
 def set_log_handlers(level:int=log_level, path=log_path):
     """Should be run if log_path or log_level is changed."""
     file_handler = RotatingFileHandler(str(log_path), 'a', 10000000, 10)
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(formatter)
-    pckg_logger.addHandler(file_handler) 
+    pckg_logger.addHandler(file_handler)
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.INFO)
     stream_handler.setFormatter(formatter)
     pckg_logger.addHandler(stream_handler)
+
 
 def set_log_path(path):
     log_path = path
@@ -47,6 +41,10 @@ def set_log_path(path):
         pckg_logger.removeHandler(hdlr)
     set_log_handlers(log_level, log_path)
 
+
 set_log_handlers(log_level, log_path)
 
-__all__ = ["geometry", "homog2d", "materials", "mesh_generate_2D", "mesh_tools", "part", "full_scale_pb", "toolbox_FEniCS", "set_log_path","set_log_handlers","log_level","log_path", "pckg_logger", "GEO_TOLERANCE"]
+__all__ = ["geometry", "homog2d", "materials", "mesh_generate_2D",
+           "mesh_tools", "part", "full_scale_pb", "toolbox_FEniCS",
+           "set_log_path", "set_log_handlers", "log_level", "log_path",
+           "pckg_logger", "GEO_TOLERANCE"]
