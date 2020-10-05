59a4d9c2511f9173bfaa4e80d4286abc10d7c670
Baptiste Durand <baptiste.durand@enpc.fr>
2020-10-05 20:13:19 +0200

    New unit test for periodic boundary condition

--- test/periodicity_test.py
+++ test/periodicity_test.py
@@ -12,8 +12,8 @@ from pytest import approx
 logger = logging.getLogger("Test_periodicity")
 logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter(
-    '%(asctime)s :: %(levelname)s :: %(name)s :: %(message)s',
-    "%H:%M:%S")
+    "%(asctime)s :: %(levelname)s :: %(name)s :: %(message)s", "%H:%M:%S"
+)
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.DEBUG)
 stream_handler.setFormatter(formatter)
@@ -22,19 +22,37 @@ logger.addHandler(stream_handler)
 
 def test_pbc_from_vectors():
     per_vect = np.array([[4.0, 0.0], [0.0, 8.0]])
-    pbc = periodicity.PeriodicDomain.pbc_dual_base(per_vect, 'XY')
+    pbc = periodicity.PeriodicDomain.pbc_dual_base(per_vect, "XY")
     test_points = [
-        (0., 0.), (4., 0.), (4., 8.), (0., 8.),
-        (2., 0.), (4., 4.), (2., 8.), (0., 4.),
+        (0.0, 0.0),
+        (4.0, 0.0),
+        (4.0, 8.0),
+        (0.0, 8.0),
+        (2.0, 0.0),
+        (4.0, 4.0),
+        (2.0, 8.0),
+        (0.0, 4.0),
     ]
     test_points = [np.array(coord) for coord in test_points]
     inside_results = [
-        True, False, False, False,
-        True, False, False, True,
+        True,
+        False,
+        False,
+        False,
+        True,
+        False,
+        False,
+        True,
     ]
     map_results = [
-        (39996, 79992), (0., 0.), (0., 0.), (0., 0.),
-        (39996, 79992), (0., 4.), (2., 0.), (39996, 79992),
+        (39996, 79992),
+        (0.0, 0.0),
+        (0.0, 0.0),
+        (0.0, 0.0),
+        (39996, 79992),
+        (0.0, 4.0),
+        (2.0, 0.0),
+        (39996, 79992),
     ]
     map_results = [np.array(coord) for coord in map_results]
     for point, result in zip(test_points, inside_results):
@@ -48,4 +66,48 @@ def test_pbc_from_vectors():
         assert y == approx(map_pt)
 
 
-test_pbc_from_vectors()
+def test_pbc_from_vectors_parallelogram():
+    per_vect = np.array([[4.0, 2.0], [0.0, 4.0]])
+    pbc = periodicity.PeriodicDomain.pbc_dual_base(per_vect, "XY")
+    test_points = [
+        (0, 0),
+        (2, 0),
+        (4, 0),
+        (1, 2),
+        (5, 2),
+        (2, 4),
+        (4, 4),
+        (6, 4),
+    ]
+    test_points = [np.array(coord, np.float64) for coord in test_points]
+    inside_results = [
+        True,
+        True,
+        False,
+        True,
+        False,
+        False,
+        False,
+        False,
+    ]
+    for point, result in zip(test_points, inside_results):
+        logger.debug(f"Point : {point}, master : {pbc.inside(point, True)}")
+        assert pbc.inside(point, on_boundary=True) == result
+
+    map_results = [
+        (59994, 39996),
+        (59994, 39996),
+        (0, 0),
+        (59994, 39996),
+        (1, 2),
+        (0, 0),
+        (2, 0),
+        (0, 0),
+    ]
+    map_results = [np.array(coord, np.float64) for coord in map_results]
+    y = np.zeros(2)
+    for point, map_pt in zip(test_points, map_results):
+        pbc.map(point, y)
+        logger.debug(f"Point : {point}, map pt : {y}, expected : {map_pt}")
+        assert y == approx(map_pt)
+    # * OK, test réussi
